9b542af1bbad40c9f19accd064bc129ff2a6e52f
J. Nathan Matias <natematias@gmail.com>
2013-08-24 15:38:12 -0700

    updated for Media Lab students 2013-2015

--- dest/mugcakes-templates.js
+++ dest/mugcakes-templates.js
@@ -6,48 +6,28 @@ var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
 __p += '<div class="cake">\n  <div class="details">\n\n    <h3><span class="bold">' +
-((__t = ( cake['Mugcake name'] )) == null ? '' : __t) +
-'</span></h3>\n    <div class="by">\n      By <a href="http://twitter.com/' +
+((__t = ( cake['Name'] )) == null ? '' : __t) +
+'</span></h3>\n\n    ';
+ if (cake['Description'] !== null) { ;
+__p += '\n      <span class="detail">' +
+((__t = ( cake['Description'] )) == null ? '' : __t) +
+'</span>\n    ';
+ } ;
+__p += '\n    \n    <span class="detail">\n      Group : <strong>' +
+((__t = ( cake['Group'] )) == null ? '' : __t) +
+'</strong>\n    </span>\n    <span class="detail">\n      Twitter: <strong><a href="http://twitter.com/' +
 ((__t = ( cake['Twitter account'].slice(1) )) == null ? '' : __t) +
 '">' +
 ((__t = ( cake['Twitter account'] )) == null ? '' : __t) +
-'</a>\n    </div>\n    <hr>\n\n    <span class="detail rating ' +
-((__t = ( cake['Rating'] >= 7 ? 'good' : (cake['Rating'] > 3 ? 'medium' : 'bad') )) == null ? '' : __t) +
-'">\n      Rating: <span class="stars">' +
-((__t = ( cake['Rating'] )) == null ? '' : __t) +
-'</span>\n    </span>\n    ';
- if (cake['Qualitative Yumminess'] !== null) { ;
-__p += '\n      <span class="detail">Qualitative Yumminess: ' +
-((__t = ( cake['Qualitative Yumminess'] )) == null ? '' : __t) +
-'</span>\n    ';
- } ;
-__p += '\n    \n    <span class="detail">\n      Cook time: ' +
-((__t = ( cake['Microwave Time'] )) == null ? '' : __t) +
-' \n      ';
- if (cake['Microwave Wattage']) { ;
-__p += '\n        in a ' +
-((__t = ( cake['Microwave Wattage'] )) == null ? '' : __t) +
-' microwave.\n      ';
- } ;
-__p += '\n    </span>\n\n    <h3>Ingredients</h3>\n    ';
- _.each(cake, function(val, key) { ;
-__p += '\n      ';
- if (controls.indexOf(key) === -1 && val !== null) { ;
-__p += '\n        <span class="detail">' +
-((__t = ( key )) == null ? '' : __t) +
-': ' +
-((__t = ( val )) == null ? '' : __t) +
-'</span>\n      ';
- } ;
-__p += '\n    ';
- }); ;
-__p += '\n\n    ';
- if (cake['Instructions'] !== null) { ;
-__p += '\n      <h3>Instructions</h3>  \n      <p>' +
-((__t = ( cake['Instructions'] )) == null ? '' : __t) +
-'</p>\n    ';
- } ;
-__p += '\n  </div>\n\n  <div class="photo">\n    ';
+'</a> </strong>\n    </span>\n    <span class="detail">\n      Email: <strong>' +
+((__t = ( cake['Email'] )) == null ? '' : __t) +
+' </strong>\n    </span>\n    <span class="detail">\n      Website: <strong><a href="' +
+((__t = (cake['Website'])) == null ? '' : __t) +
+'">' +
+((__t = (cake['Website'])) == null ? '' : __t) +
+'</a></strong>\n    </span>\n    <span class="detail">\n      What I can teach: <strong>' +
+((__t = (cake['What I Can Teach'])) == null ? '' : __t) +
+'</strong>\n    </span>\n  </div>\n  <div class="photo">\n    ';
  if (cake['Photo']) { ;
 __p += '\n      <img src="' +
 ((__t = ( cake['Photo'] )) == null ? '' : __t) +
@@ -55,7 +35,7 @@ __p += '\n      <img src="' +
  } else { ;
 __p += '\n      <img src="img/no-image.jpg" width="300" alt=""/>\n    ';
  } ;
-__p += '\n  </div>\n</div>';
+__p += '\n  </div>\n</div>\n';
 
 }
 return __p
--- dest/mugcakes.js
+++ dest/mugcakes.js
@@ -1,36 +1,22 @@
 /* global $,Miso,JST */
 $(function(){
 
-  $.fn.stars = function(max) {
-    return $(this).each(function() {
-      // Get the value
-      var val = +($(this).html());
-      // Make sure that the value is in 0 - 5 range, multiply to get width
-      var size = val * 16;
-      // Create stars holder
-      var $span = $("<span />").width(size);
-      // Replace the numerical value with stars
-      $(this).html($span);
-    });
-  };
-
   var cakes = new Miso.Dataset({
     importer : Miso.Dataset.Importers.GoogleSpreadsheet,
     parser : Miso.Dataset.Parsers.GoogleSpreadsheet,
-    key : "0Al5UYaVoRpW3dEZPaG1QMWl5NTBXNGdqZkVEZmE2eEE",
+    key : "0AjwGdn_H2ROpdE1ISVFSelBqc3RRTVZYV3FnSXRUcmc",
     worksheet: "2"
   });
 
   var workingColumns = [
     "_id",
-    "Mugcake name",
-    "Twitter account",
-    "Photo",
-    "Rating",
-    "Qualitative Yumminess",
-    "Instructions",
-    "Microwave Wattage",
-    "Microwave Time"
+    "Name",
+    "Group",
+    "Email",
+    "Twitter",
+    "Website",
+    "Description",
+    "Photo"
   ];
 
   var $el = $("#cakes");
@@ -41,8 +27,6 @@ $(function(){
         controls : workingColumns
       }));
     });
-
-    $(".stars").stars(10);
   });
 
   
